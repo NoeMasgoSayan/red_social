f2c094a37012f7bbb68fac95ee507ce3450f4e0d
Noelia <masgonoelia21@gmail.com>
2024-12-06 01:21:04 -0500

    saludo

--- assets/js/checkLogin.js
+++ assets/js/checkLogin.js
@@ -1,12 +1,24 @@
 const navbarBtns = document.querySelector("#navbar-btns");
 const cardInicioSesion = document.querySelector("#cardInicioSesion");
+const mainContainer = document.querySelector("#main-container");
+const saludo = document.querySelector("#saludo");
 
 export const checkLogin = (user) => {
+  // Modificamos el nav dependiendo si el usuario está logeado o no
   if (user) {
     navbarBtns.style.display = "block";
     cardInicioSesion.style.display = "none";
+
+    // Mostramos el main container
+    mainContainer.style.display = "block";
+    saludo.textContent = `Bienvenid@ ${user.email}`;
+    console.log("saludo");
   } else {
     navbarBtns.style.display = "none";
     cardInicioSesion.style.display = "block";
+
+    // Ocultamos el main container
+    mainContainer.style.display = "none";
+    saludo.textContent = "";
   }
 };
